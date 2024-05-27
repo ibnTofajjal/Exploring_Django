e26c9f45ca049ae8c2c94b7cdb0d8cfa6c494bb2
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-05-27 14:34:19 +0600

    Learn: Django Template Language

--- --- NAVIGATION ---/navigation_project/navigation/views.py
+++ --- NAVIGATION ---/navigation_project/navigation/views.py
@@ -6,11 +6,24 @@ def navigation(request):
     return render("Hello! This is --HOME-- Of the Navigation Directory")
 
 def about(request):
-    my_about = {
-        'Name': 'JY-97',
-        'Age': '24',
-        'Phone': '01754623158'
-    }
+    my_about = {'author' : 'Rahim', 'age' : 5, 'lst' : ['python','is','best'], 'birthday' : datetime.datetime.now(), 'val' : '' ,'courses' : [
+        
+        {
+            'id' : 1,
+            'name' : 'Python',
+            'fee' : 5000
+        },
+        {
+            'id' : 2,
+            'name' : 'Django',
+            'fee' : 10000 
+        },
+        {
+            'id' : 3,
+            'name' : 'C',
+            'fee' : 1000 
+        },
+    ]}
     return render(request, "navigation/about.html", context=my_about)
 
 def contact(request):
