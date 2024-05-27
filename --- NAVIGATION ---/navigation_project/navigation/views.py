795326b655fe82746172127d7b486d0be0b18bb9
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-05-27 14:27:01 +0600

    Learn Context

--- --- NAVIGATION ---/navigation_project/navigation/views.py
+++ --- NAVIGATION ---/navigation_project/navigation/views.py
@@ -6,7 +6,12 @@ def navigation(request):
     return render("Hello! This is --HOME-- Of the Navigation Directory")
 
 def about(request):
-    return render(request, "navigation/about.html")
+    my_about = {
+        'Name': 'JY-97',
+        'Age': '24',
+        'Phone': '01754623158'
+    }
+    return render(request, "navigation/about.html", context=my_about)
 
 def contact(request):
     return render(request, "navigation/contact.html")
