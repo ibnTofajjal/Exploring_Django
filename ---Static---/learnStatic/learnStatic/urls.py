db179639a157e2cca66543c2074fe25842ea24f5
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-05-29 20:09:02 +0600

    Local-[ inside the app ] Static File Render

--- ---Static---/learnStatic/learnStatic/urls.py
+++ ---Static---/learnStatic/learnStatic/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+
+    # App Permission
+    path('github/', include('staticApp.urls'))
 ]
