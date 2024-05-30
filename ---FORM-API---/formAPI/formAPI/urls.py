c24f27549e406b00b0a51fd13167e16c21cf5dac
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-05-30 20:43:15 +0600

    Basic initialization

---
+++ ---FORM-API---/formAPI/formAPI/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+
+    #APP Access
+    path('about/', include('form_APP.urls'), name='about')
+
+]
