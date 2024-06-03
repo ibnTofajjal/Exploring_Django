ae9fb102ccc498d930b001646a8a9f308279b97d
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-06-03 21:47:13 +0600

    Delete Operation From the Template

--- ---MODELS---/LearnModel/App01/urls.py
+++ ---MODELS---/LearnModel/App01/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name="homepage"),
+    path('delete/<int:id>', views.delete_student, name="delete_student")
 
 ]
--- ---MODELS---/LearnModel/App01/views.py
+++ ---MODELS---/LearnModel/App01/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 # Create your views here.
 def home(request):
+    data = models.Students.objects.all()
+    return render(request, 'home.html',{"data": data})
+
+
+def delete_student(request, id):
+    targeted_student = models.Students.objects.get(pk = id).delete()
+    # return redirect("homepage")
+    print(targeted_student)
     data = models.Students.objects.all()
     return render(request, 'home.html',{"data": data})
